0022cc18dc9635adf25e4e6d61b71be0ba91b9fe
tamtech10 <tamtech.dev@gmail.com>
2025-10-27 16:58:28 +0100

    numbers from 0 to 1337 inclusive EXCEPT 26, 37, 88 and 1111.

---
+++ Mini projets/Job02/src/job02.java
@@ -0,0 +1,13 @@
+public class job02 {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 1337; i++) {
+            if (i==26 || i==37 || i==88 || i==1111) {
+                System.out.println();
+            } else {
+                System.out.println(i);
+            }
+            }
+        }
+}
