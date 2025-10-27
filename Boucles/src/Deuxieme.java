eca18ec878f9822e86e041b8f3c4fc93d84a84ad
tamtech10 <tamtech.dev@gmail.com>
2025-10-27 15:54:42 +0100

    from 10 to 0 backwards

---
+++ Boucles/src/Deuxieme.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Deuxieme {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        for (int i = 10; i >= 0; i--) {
+            System.out.println(i);
+        }
+        }
+}
