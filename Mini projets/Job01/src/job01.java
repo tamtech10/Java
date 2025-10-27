8e8d1eddd8d167a0117030dcbced6413bd2ab816
tamtech10 <tamtech.dev@gmail.com>
2025-10-27 16:44:32 +0100

    numbers between 0 and 1337. The number 42 highlighted

---
+++ Mini projets/Job01/src/job01.java
@@ -0,0 +1,13 @@
+public class job01 {
+
+        public static void main(String[] args) {
+
+            for (int i = 0; i <= 1337; i++) {
+                if (i == 42) {
+                    System.out.println(" Numero " + i);
+                } else {
+                    System.out.println(i);
+                }
+            }
+        }
+}
