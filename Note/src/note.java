8acc98d062fd9385631a41621f728f4273504ae6
tamtech10 <tamtech.dev@gmail.com>
2025-10-27 13:43:51 +0100

    note entre 0 et 20

--- Note/src/note.java
+++ Note/src/note.java
@@ -6,6 +6,19 @@ public class note {
 
         Scanner input = new Scanner(System.in);
 
+        System.out.println("Une note entre 0 et 20");
+        int nombre = input.nextInt();
+
+        if ( nombre>=16 ) {
+            System.out.println("Très bien");
+
+            } else if (nombre >= 12) {
+                System.out.println("Bien");
+            } else if (nombre >= 10) {
+                System.out.println("Assez bien");
+            }   else {
+            System.out.println("Insuffisant");
+            }
 
     }
 }
