65dd5a1022bd1de9a903d0d3dc899b2c40a7a3cd
tamtech10 <tamtech.dev@gmail.com>
2025-10-27 16:04:11 +0100

    even numbers up to 20.

---
+++ Boucles/src/Troisieme.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Troisieme {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        for (int i = 0; i <= 20; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+
+}
