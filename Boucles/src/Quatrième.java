397120b4c6f37c7ea9451614553013cb607a5c66
tamtech10 <tamtech.dev@gmail.com>
2025-10-27 16:23:54 +0100

    la somme des nombres de 1 à 100

---
+++ Boucles/src/Quatrième.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Quatrième {
+
+    public static void main(String[] args) {
+
+        int somme = 0;
+        for( int i=1; i<=100; i++) {
+
+            System.out.println(somme);
+            System.out.println(i);
+            somme= somme+i;
+
+        }
+        System.out.println(somme);
+
+        // 5050
+    }
+}
