2d634eabe05dd636fa868ded452d66380959c714
tamtech10 <tamtech.dev@gmail.com>
2025-10-27 13:23:39 +0100

    with else/ else if

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -4,7 +4,8 @@ public class Calculator {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Choisissez une opération:");
+
+        /*System.out.println("Choisissez une opération:");
         System.out.println("1.Addition:");
         System.out.println("2.Soustraction:");
         System.out.println("3.Multiplication:");
@@ -56,6 +57,44 @@ public class Calculator {
 
             int resultat = valeur1/valeur2;
 
-            System.out.println(resultat);}
+            System.out.println(resultat);}*/
+
+        System.out.println(" une simple calculatrice ");
+
+        System.out.println( "entrer votre 1er nombre: ");
+        double nb1 = input.nextDouble();
+
+        System.out.println( "entrer votre 2éme nombre: ");
+        double nb2 = input.nextDouble();
+
+        System.out.println( "entrer l'opérateur désiré(  +, -, * , /): ");
+        char op = input.next().charAt(0);
+
+        double resultat;
+        if ( op == '+'){
+            resultat= nb1  + nb2;
+            System.out.println("le résultat est: " + resultat);}
+
+            else if (op == '-') {
+            resultat= nb1  - nb2;
+            System.out.println("le résultat est: " + resultat);}
+
+            else if (op == '*') {
+            resultat= nb1  * nb2;
+            System.out.println("le résultat est: "+ resultat);}
+
+            else if (op == '/') {
+
+                if (nb2 != 0) {
+                resultat = nb1 / nb2;
+                System.out.println("le résultat est: "+ resultat);}
+                 else {
+                System.out.println("erreur");}
+
+        }
+                else {
+                System.out.println("Char not valid");}
+
+
     }
 }
