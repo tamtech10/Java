bd44ff44fe9a1096c3e5a4f1c93369f2f9349f88
tamtech10 <tamtech.dev@gmail.com>
2025-10-27 15:47:59 +0100

    from 1 to 10

---
+++ Boucles/src/Premiere.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Premiere {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
