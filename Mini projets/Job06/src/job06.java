1b32d90b429c050d13553eea2539b711549a69e3
tamtech10 <tamtech.dev@gmail.com>
2025-10-28 14:38:17 +0100

    a rectangle 20 wide by 10 high

---
+++ Mini projets/Job06/src/job06.java
@@ -0,0 +1,19 @@
+public class job06 {
+
+    public static void main(String[] args) {
+
+        int largeur = 20;
+        int hauteur = 10;
+
+        for (int i = 1; i <= hauteur ; i++) {
+            for (int a = 1; a <= largeur; a++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+
+        }
+    }
+}
+
+
