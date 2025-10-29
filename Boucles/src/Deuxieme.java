03df0c9db76dcc94fbd9545efb7f49ee5f27be5c
tamtech10 <tamtech.dev@gmail.com>
2025-10-29 13:37:52 +0100

    numbers from 10 to 0 (countdown).

---
+++ Boucles/src/Deuxieme.java
@@ -0,0 +1,9 @@
+public class Deuxieme {
+
+    public static void main(String[] args) {
+
+        for (int i = 10; i >= 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ Rock.Paper.Scissors/src/scissors.java
@@ -0,0 +1,2 @@
+public class scissors {
+}
