9187c939ada7fb79f2b8ba40dd29d0167541f1fd
tamtech10 <tamtech.dev@gmail.com>
2025-10-30 14:37:14 +0100

    Job01 array

---
+++ Tableaux/src/Job01.java
@@ -0,0 +1,18 @@
+public class Job01 {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {200, 204, 173, 98, 171, 404, 459};
+
+        for (int i = 0; i < numbers.length; i++) {
+
+            int nb = numbers [i];
+
+            if ( nb % 2 == 0) {
+                System.out.println( nb +  "est pair");
+            } else {
+                System.out.println( nb + "est impair");
+            }
+        }
+    }
+}
