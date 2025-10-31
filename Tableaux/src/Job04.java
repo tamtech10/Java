9dc2bd6c64c7d7eda06ea9a2366d5e81a3945fe8
tamtech10 <tamtech.dev@gmail.com>
2025-10-31 10:49:58 +0100

    Job04 the total number of characters in the string

--- Tableaux/src/Job04.java
+++ Tableaux/src/Job04.java
@@ -3,5 +3,9 @@ public class Job04 {
     public static void main(String[] args) {
 
         String str = " Dans l'espace, personne ne vous entend crier";
+
+        int nombre = str.length();
+
+        System.out.println("le nombre total de caractères est: " + nombre);
     }
 }
