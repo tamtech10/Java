61b6fbab2d4f6172b55a92034fff1dbadd53573c
tamtech10 <tamtech.dev@gmail.com>
2025-10-31 13:44:14 +0100

    Job06 chain upside down

--- Tableaux/src/Job06.java
+++ Tableaux/src/Job06.java
@@ -1,2 +1,11 @@
 public class Job06 {
+
+    public static void main(String[] args) {
+
+        String str = "Les choses que l'on possède finissent par nous posséder.";
+
+        for (int i = str.length() - 1 ; i >= 0; i--) {
+            System.out.print(str.charAt(i));
+        }
+    }
 }
