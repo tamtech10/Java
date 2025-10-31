5703b2d87083b4ecb3c491b279cafdf3e6ec5225
tamtech10 <tamtech.dev@gmail.com>
2025-10-31 10:45:07 +0100

    comments

--- Tableaux/src/Job02.java
+++ Tableaux/src/Job02.java
@@ -5,6 +5,18 @@ public class Job02 {
         String str = "Tous ces instants seront perdus dans le temps comme les larmes sous la pluie.";
         char[] array = str.toCharArray();
 
+        //nacin 1
+        //int  count = 1;
+        //for (char C :array) {
+        //if (count % 2 !=0) {
+        // System.out.println(c);
+
+        //nacin 2
+        //for (int i = 0; i < array.length; i++) {
+        // if ((i % 2) == 0)
+        //System.out.print(array[i])
+
+        //nacin 3
         for (int i = 0; i < array.length; i+=2 ) {
                 System.out.print(array[i]);
             }
---
+++ Tableaux/src/Job04.java
@@ -0,0 +1,7 @@
+public class Job04 {
+
+    public static void main(String[] args) {
+
+        String str = " Dans l'espace, personne ne vous entend crier";
+    }
+}
