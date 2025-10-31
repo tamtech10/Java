3f03095c719f81a1b568c628d70f8952dcc5feaa
tamtech10 <tamtech.dev@gmail.com>
2025-10-31 15:29:12 +0100

    Job07 each character is replaced by the next one.

---
+++ Tableaux/src/Job07.java
@@ -0,0 +1,14 @@
+public class Job07 {
+
+    public static void main(String[] args) {
+
+        String str = "Certaines choses changent, et d'autres ne changeront jamais.";
+        char[] array = str.toCharArray();
+
+        for (int i = 1; i < array.length; i++) {
+
+            System.out.print(array[i]);
+        }
+            System.out.println("C");
+    }
+}
