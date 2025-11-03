1281cdec20cf0c3eb15e96729c5fb8ab542d03bb
tamtech10 <tamtech.dev@gmail.com>
2025-11-03 16:51:48 +0100

    Job08 changes

--- Tableaux/src/Job08.java
+++ Tableaux/src/Job08.java
@@ -19,4 +19,36 @@ public class Job08 {
 
     }
     }
+// Arrays.sort
 
+
+ //public class JobTab8 {
+//    public static void main(String[] args) {
+//        // sort the array elements from smallest to largest
+//
+//        int[] tab = {123, 0, 98, 171, 3, 459, 67};
+//        System.out.println(" Sort the Array Element from Smallest to Largest: ");
+//
+//        for (int i = 0; i < tab.length; i++) { // browse the tab
+//            int smallest = i; //define the index i as the smallest by default
+//            for (int j = i + 1; j < tab.length; j++) // j the index for smallest
+//            {
+//                if (tab[j] < tab[smallest]) //we compare the smallest to the new index
+//                {
+//                    smallest = j;   // if true so the index j become the new smallest
+//
+//                }
+//            }
+//            int order; // the new variable for the smallest
+//            order = tab[i]; //put the variable into the first tab
+//            tab[i]=tab[smallest]; // we define as the smallest
+//            tab[smallest]= order; //we attribute the value of smallest to order
+//            for (int x : tab) {
+//                System.out.print(x+" ");
+//            }
+//            System.out.println();
+//        }
+//
+//        for (int x : tab) {
+//            System.out.print(x+" ");
+//        }
