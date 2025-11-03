9391ef14f9c2a4bcad040e0072157641a203b9a6
tamtech10 <tamtech.dev@gmail.com>
2025-11-03 11:26:27 +0100

    Job03 changes

--- Tableaux/src/Job03.java
+++ Tableaux/src/Job03.java
@@ -5,6 +5,13 @@ public class Job03 {
         String str = "I'm sorry Dave I'm afraid I can't do that";
         char[] array = str.toCharArray();
 
+
+        // char [] voyelles = {'A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u', 'Y', 'y'};
+        // for (char c : array) {
+        // for (char voyelle : voyelles) {
+        // if (c == voyelle) {
+        // System.out.println(c);
+
         for (char c : array) {
             if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'I' || c == 'y') {
                 System.out.print(c);
