f3478b43907a32401f68b9303c7158e6df22546c
tamtech10 <tamtech.dev@gmail.com>
2025-11-03 11:39:56 +0100

    Job04 changes

--- Tableaux/src/Job04.java
+++ Tableaux/src/Job04.java
@@ -4,6 +4,42 @@ public class Job04 {
 
         String str = " Dans l'espace, personne ne vous entend crier";
 
+        //prvi nacin
+        // char[] strArray = str.toCharArray();
+        // int espace = 0;
+        // for (char a : chars) {
+        // if (a == ' ') {
+        // a = 1;
+        // espace += a;
+        // }
+        // }
+        // System.out.println(chars.length - espace)
+
+        //drugi nacin
+        // char[] tab = str.toCharArray();
+        // int somme = 0;
+        // for(int i = 0; i < tab.length; i++) {
+        // somme = i+1;
+        // }
+        // System.out.println(+somme);
+        //}
+
+
+        //treci nacin
+        // char[] strArray = str.toCharArray();
+        // int total = 0;
+        // for (char c : strArray) {
+        // if (c == ' ' || c == ',' || c == '\'') {
+        // continue;
+        //  }
+        // if (c == ' ') {
+        // continue;
+        // }
+        // total++;
+        // }
+        // System.out.println(total);
+
+        //cetvrti nacin:
         int nombre = str.length();
 
         System.out.println("le nombre total de caractères est: " + nombre);
