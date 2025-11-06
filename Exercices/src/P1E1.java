cd2fde2f0263a6bc29c68d840d5b588f27261440
tamtech10 <tamtech.dev@gmail.com>
2025-11-06 09:43:48 +0100

    P1E1 Multiples of 3 between 1 and 50.

---
+++ Exercices/src/P1E1.java
@@ -0,0 +1,18 @@
+public class P1E1 {
+
+    public static void main (String[] args) {
+
+    multiples();
+    }
+
+    public static void multiples() {
+
+        for (int i = 1; i <= 50; i++) {
+            if (i % 3 == 0) {
+                System.out.println(i);
+            }
+
+        }
+
+    }
+}
