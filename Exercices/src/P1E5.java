396b0f9d87eb54f38c3d6a79f96fb99a8739e5b8
tamtech10 <tamtech.dev@gmail.com>
2025-11-06 14:06:09 +0100

    P1E5 damier

--- Exercices/src/P1E5.java
+++ Exercices/src/P1E5.java
@@ -1,6 +1,30 @@
+import java.util.Scanner;
+
 public class P1E5 {
 
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Choisissez un nombre: ");
+        int n = scan.nextInt();
+        nDamier(n);
+
+    }
+    public static void nDamier(int n) {
+
+        for(int row = 0; row < n; row++) {
+
+            for(int col = 0; col < n; col++) {
+
+                if ((row + col) % 2 == 0) {
+                    System.out.print("#");
+                } else {
+                    System.out.print("-");
+                }
+            }
+            System.out.println();
+        }
+
     }
 }
+
