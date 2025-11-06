f106941b7fa9926160b1d17bb53a0205bafe7c76
tamtech10 <tamtech.dev@gmail.com>
2025-11-06 11:56:49 +0100

    P1E4 Factorial n!

---
+++ Exercices/src/P1E4.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class P1E4 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Choisissez un nombre: ");
+        int choix = scan.nextInt();
+        factorielle(choix);
+
+    }
+    public static void factorielle(int n) {
+        int fact = 1;
+        for(int i = 1; i <= n; i++) {
+            fact = fact * i;
+        }
+        System.out.println(fact);
+    }
+}
