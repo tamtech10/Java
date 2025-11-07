6193752c382b97ef7b92c4b1887f4d2ea2238134
tamtech10 <tamtech.dev@gmail.com>
2025-11-07 09:34:21 +0100

    P1E7 sum of the entered numbers

--- Exercices/src/P2E7.java
+++ Exercices/src/P2E7.java
@@ -5,19 +5,23 @@ public class P2E7 {
     public static void main(String[] args) {
 
         int resultat = additionner();
-        System.out.println("La somme totale est : " + resultat);
+        System.out.println("La somme totale de nombres choisis est : " + resultat);
 
     }
 
     public static int additionner () {
         Scanner scan = new Scanner(System.in);
-        int numero = 0;
+
+        int numero;
         int somme = 0;
 
+        System.out.println("Saisissez le numéro (0 pour annuler) ");
+        numero = scan.nextInt();
+
         while ( numero != 0 ) {
-            System.out.println("Saisissez le numéro (0 pour annuler) ");
-            numero = scan.nextInt();
             somme += numero;
+            System.out.println("Saisissez le numéro");
+            numero = scan.nextInt();
         }
         scan.close();
         return somme;
