9a24e27a2724e36dd19cd3b8306ca3218375e6e1
tamtech10 <tamtech.dev@gmail.com>
2025-11-10 13:47:38 +0100

    P2E15 8 elements, show the minimum and position

---
+++ Exercices/src/P4E15.java
@@ -0,0 +1,24 @@
+public class P4E15 {
+
+    static void main(String[] args) {
+
+        int[] nombres = { -3, 1, -7, 2, -11, 9, 13, -5};
+        int[] resultat = trouverMin(nombres);
+        System.out.println("Le plus petit nombre est : " + resultat[0]);
+        System.out.println("Son index dans le tableau est : " + resultat[1]);
+
+    }
+
+    public static int [] trouverMin(int[] tableau) {
+
+        int min = tableau[0];
+        int position = 0;
+
+        for (int i = 1; i < tableau.length; i++) {
+            if (tableau[i] < min) {
+                min = tableau[i];
+                position = i;
+            }
+        } return new int[] {min, position};
+    }
+}
