51bf8d627371ce2050ff3106b7cc009a617c4c64
tamtech10 <tamtech.dev@gmail.com>
2025-11-12 14:53:48 +0100

    P2E22 changes

--- Exercices/src/P5E22.java
+++ Exercices/src/P5E22.java
@@ -8,7 +8,7 @@ public class P5E22 {
         int[] tableau1 = {7, 8, 9, 10, 11};
         int[] tableau2 = copierTab(tableau1);
 
-        System.out.println("Premier tableau : " + Arrays.toString(tableau1));
+        System.out.println("Premier tableau : " + Arrays.toString(tableau1)); //method for converting Java string array to string
         System.out.println("Deuxiem tableau : " + Arrays.toString(tableau2));
 
     }
