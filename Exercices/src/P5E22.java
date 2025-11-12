23e3e05dce94714628b301d5df4f362194fc0f4a
tamtech10 <tamtech.dev@gmail.com>
2025-11-12 14:51:33 +0100

    P2E22 copied first array into second

---
+++ Exercices/src/P5E22.java
@@ -0,0 +1,25 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class P5E22 {
+
+    public static void main (String[] args) {
+
+        int[] tableau1 = {7, 8, 9, 10, 11};
+        int[] tableau2 = copierTab(tableau1);
+
+        System.out.println("Premier tableau : " + Arrays.toString(tableau1));
+        System.out.println("Deuxiem tableau : " + Arrays.toString(tableau2));
+
+    }
+
+    public static int[] copierTab (int[] tab1) {
+
+        int[] copie = new int[tab1.length];
+
+        for (int i = 0; i < tab1.length; i++) {
+            copie[i] = tab1[i];
+        }
+        return copie;
+    }
+}
