fb6ca58d166439fc6d4aa0b54719daf0d6e7a212
tamtech10 <tamtech.dev@gmail.com>
2025-11-13 16:14:51 +0100

    Exo1 class, two attributes, object.

---
+++ Classes/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+       Personne unePersonne = new Personne();
+
+       unePersonne.nom = "Tamara";
+       unePersonne.age = 30;
+
+        System.out.println("Mon nom est " + unePersonne.nom + ";");
+        System.out.println("J'ai " + unePersonne.age + " ans.");
+    }
+
+}
---
+++ Classes/src/Personne.java
@@ -0,0 +1,6 @@
+public class Personne {
+
+    String nom;
+    int age;
+
+}
