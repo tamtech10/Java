b681a73eab46d2fc3041227ec45f5a5b1212b89a
tamtech10 <tamtech.dev@gmail.com>
2025-11-13 16:30:31 +0100

    Exo2 in the class one method called in main

---
+++ Classes/src/job02/Main.java
@@ -0,0 +1,15 @@
+package job02;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Personne unePersonne = new Personne();
+
+        unePersonne.nom = "Tamara";
+        unePersonne.age = 30;
+        unePersonne.sePresenter();
+
+    }
+
+}
---
+++ Classes/src/job02/Personne.java
@@ -0,0 +1,15 @@
+package job02;
+
+public class Personne {
+
+        String nom;
+        int age;
+
+        public void sePresenter() {
+
+            System.out.println("Je m'appelle " + nom + "j'ai " + age + " ans.");
+        }
+
+    }
+
+
