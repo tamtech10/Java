bfbd42d9b85ab229c746f2178f2ff486e23363b8
tamtech10 <tamtech.dev@gmail.com>
2025-11-14 16:47:08 +0100

    Exo10 The area of the rectangle

---
+++ Classes/src/job10/Main.java
@@ -0,0 +1,12 @@
+package job10;
+
+public class Main {
+
+    static void main(String[] args) {
+
+        Rectangle r = new Rectangle(10, 7);
+
+        r.calculerAire();
+        System.out.println("L'aire du rectangle est : " + r.calculerAire());
+    }
+}
---
+++ Classes/src/job10/Rectangle.java
@@ -0,0 +1,19 @@
+package job10;
+
+public class Rectangle {
+
+    double largeur;
+    double hauteur;
+
+    public Rectangle(double largeur, double hauteur) {
+        this.largeur = largeur;
+        this.hauteur = hauteur;
+    }
+
+    public double calculerAire() {
+
+        return largeur * hauteur;
+        }
+    }
+
+
