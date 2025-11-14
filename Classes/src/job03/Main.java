d77fecf76c24d0dfef6659ca9887fa005f6be272
tamtech10 <tamtech.dev@gmail.com>
2025-11-14 10:08:45 +0100

    Exo3 one constructor in class, two new objets in main

---
+++ Classes/src/job03/Main.java
@@ -0,0 +1,12 @@
+package job03;
+
+public class Main {
+
+    static void main(String[] args) {
+
+        Personne unePersonne = new Personne("Mia", 40);
+        Personne p2 = new Personne("Maria", 25);
+        Personne p3 = new Personne("Marco", 15);
+    }
+
+}
---
+++ Classes/src/job03/Personne.java
@@ -0,0 +1,15 @@
+package job03;
+
+public class Personne {
+
+    String nom;
+    int age;
+
+
+    public Personne(String nom, int age) {
+        this.nom = nom;
+        this.age = age;
+
+    }
+
+}
